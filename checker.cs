c55a8a5f984e33f431687e6f03701838eadcda0c
khushi choudhary <khushichoudhary12599@gmail.com>
2020-09-13 13:36:12 +0530

    Vital

--- checker.cs
+++ checker.cs
@@ -3,7 +3,7 @@ using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Windows.Forms
+using System.Windows.Forms;
 using System.Threading.Tasks;
 
 namespace Checkers
